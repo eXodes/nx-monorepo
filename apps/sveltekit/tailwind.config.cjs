45c795c086c328434d48c12a37e41508238cab31
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-13 01:27:45 +0800

    style(sveltekit): add brand color theme

--- apps/sveltekit/tailwind.config.cjs
+++ apps/sveltekit/tailwind.config.cjs
@@ -6,10 +6,21 @@ module.exports = {
     "./src/**/*.{html,svelte,tsx,ts,jsx,js,css}",
     "../../packages/**/*.{html,svelte,tsx,ts,jsx,js,css}",
   ],
-  options: {
-    rejected: true,
-    printRejected: true,
-    safelist: ["html", "body", "dark"],
-    safelistPatterns: [/svelte-/],
+  theme: {
+    colors: {
+      brand: {
+        DEFAULT: "#FF3E00",
+        50: "#FFC9B8",
+        100: "#FFBAA3",
+        200: "#FF9B7A",
+        300: "#FF7C52",
+        400: "#FF5D29",
+        500: "#FF3E00",
+        600: "#C73000",
+        700: "#8F2300",
+        800: "#571500",
+        900: "#1F0700",
+      },
+    },
   },
 };
