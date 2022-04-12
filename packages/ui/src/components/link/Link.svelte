396d1713c2b70fbf0f1a139f0068146c4be0fe3f
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-13 02:56:36 +0800

    feat(ui): add external indicator to `Link`

--- packages/ui/src/components/link/Link.svelte
+++ packages/ui/src/components/link/Link.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
+import { ExternalLink, Icon } from "svelte-hero-icons";
+
 export let href: string;
 export let target = "_self";
+export let external = false;
 </script>
 
 <a
-  class="text-brand-500 hover:text-brand-400 hover:underline"
+  class="inline-flex text-brand-500 hover:text-brand-400 hover:underline"
   href={href}
-  target={target}
+  target={external ? "_blank" : target}
   data-testid="link"
 >
   <slot />
+  {#if external}
+    <span class="ml-0.5 my-auto inline">
+      <Icon src={ExternalLink} class="w-4 h-4" />
+    </span>
+  {/if}
 </a>
