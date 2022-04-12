5e95d89e6f7085fa27bcf6b05cea75965ccc05d3
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-13 01:28:29 +0800

    style(ui): set `Link` to brand color

--- packages/ui/src/components/link/Link.svelte
+++ packages/ui/src/components/link/Link.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-  export let href: string;
-  export let target = "_self";
+export let href: string;
+export let target = "_self";
 </script>
 
-<a class="hover:underline" href={href} target={target} data-testid="link">
+<a
+  class="text-brand-500 hover:text-brand-400 hover:underline"
+  href={href}
+  target={target}
+  data-testid="link"
+>
   <slot />
 </a>
