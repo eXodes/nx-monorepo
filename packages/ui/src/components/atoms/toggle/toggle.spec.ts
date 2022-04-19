92b45cb2d5cfe1f4b02cc8bf5cd5b4a0d06f66fb
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-20 03:34:45 +0800

    test(ui): add toggle default value test

--- packages/ui/src/components/atoms/toggle/toggle.spec.ts
+++ packages/ui/src/components/atoms/toggle/toggle.spec.ts
@@ -3,13 +3,35 @@ import { render, fireEvent } from "@testing-library/svelte";
 
 describe("Toggle", () => {
   it("renders", () => {
-    const { container } = render(Toggle);
+    const { container } = render(Toggle, {
+      props: {
+        label: "Toggle",
+      },
+    });
 
     expect(container).toMatchSnapshot();
   });
 
+  it("renders with checked props 'true'", async () => {
+    const { container, getByTestId } = render(Toggle, {
+      props: {
+        enabled: true,
+        label: "Toggle",
+      },
+    });
+
+    const element = getByTestId("toggle");
+
+    expect(container).toMatchSnapshot();
+    expect(element.getAttribute("aria-checked")).toBe("true");
+  });
+
   it("it should toggle checked value to 'true' when clicked once", async () => {
-    const { getByTestId } = render(Toggle);
+    const { getByTestId } = render(Toggle, {
+      props: {
+        label: "Toggle",
+      },
+    });
 
     const element = getByTestId("toggle");
 
@@ -19,7 +41,11 @@ describe("Toggle", () => {
   });
 
   it("it should toggle checked value to 'false' when clicked twice", async () => {
-    const { getByTestId } = render(Toggle);
+    const { getByTestId } = render(Toggle, {
+      props: {
+        label: "Toggle",
+      },
+    });
 
     const element = getByTestId("toggle");
 
