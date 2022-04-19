dd35c112856b8b17f61712734e37d1a7c9fd61db
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-19 23:27:25 +0800

    feat(nest): import `common` module

--- apps/nest/src/app/app.module.ts
+++ apps/nest/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { CommonModule } from "@nx-monorepo/common";
 
 @Module({
-  imports: [],
+  imports: [CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
