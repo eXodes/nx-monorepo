c0b790ea77195dfc0924976fc7222d075e2e4773
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-19 23:32:00 +0800

    test(sveltekit): rename `appName` based on api response

--- apps/sveltekit/src/tests/app.spec.ts
+++ apps/sveltekit/src/tests/app.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test.describe("App", () => {
   test("should has app title", async ({ page }) => {
     await page.goto("/");
-    const appName = process.env.APP_NAME ?? "SvelteKit with NX";
+    const appName = "NX-powered Applications";
 
     expect(await page.content()).toContain(`<title>${appName}</title>`);
   });
