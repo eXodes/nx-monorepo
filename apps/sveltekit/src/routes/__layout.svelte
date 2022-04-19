b1b3e84acf0f478a42833f8c43a09e1b1b7e2537
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-19 15:51:02 +0800

    feat(sveltekit): add theme store for dark mode

--- apps/sveltekit/src/routes/__layout.svelte
+++ apps/sveltekit/src/routes/__layout.svelte
@@ -2,8 +2,14 @@
 import "../app.css";
 
 import { Container } from "@nx-monorepo/ui";
+import { classNames } from "@nx-monorepo/utils";
+import { theme } from "@nx-monorepo/store";
 </script>
 
-<Container class="min-h-screen">
-  <slot />
-</Container>
+<main class={classNames($theme === "dark" && "dark")}>
+  <div class="duration-100 motion-reduce:transition-none dark:bg-gray-900">
+    <Container class="min-h-screen" }>
+      <slot />
+    </Container>
+  </div>
+</main>
