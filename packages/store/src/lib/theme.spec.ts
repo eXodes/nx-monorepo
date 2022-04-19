e682c8cf94e93f69fc6df69c3f5eecb05350c493
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-04-19 16:29:54 +0800

    test(store): add change theme test

--- packages/store/src/lib/theme.spec.ts
+++ packages/store/src/lib/theme.spec.ts
@@ -2,8 +2,24 @@ import theme from "./theme";
 
 describe("Theme store", () => {
   it("should have default value of 'light'", () => {
+    let current = "";
+
     theme.subscribe((value) => {
-      expect(value).toBe("light");
+      current = value;
     });
+
+    if (current) expect(current).toBe("light");
+  });
+
+  it("should be able to change theme", () => {
+    let current = "";
+
+    theme.subscribe((value) => {
+      current = value;
+    });
+
+    theme.set("dark");
+
+    if (current) expect(current).toBe("dark");
   });
 });
