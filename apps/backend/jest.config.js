db139e5050572f95d18ffb9c03e7f3c21ab58e6a
Exo Des <9076458+eXodes@users.noreply.github.com>
2022-05-15 18:41:45 +0800

    refactor(backend): move nest to backend

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: "nest",
+  displayName: "backend",
   preset: "../../jest.preset.js",
   globals: {
     "ts-jest": {
@@ -11,5 +11,5 @@ module.exports = {
     "^.+\\.[tj]s$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../coverage/apps/nest",
+  coverageDirectory: "../../coverage/apps/backend",
 };
